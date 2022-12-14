1bc2979e1352a435819d812a5f14770a9d682b15
PVBrodersen <phbro19@student.sdu.dk>
2022-12-14 15:10:20 +0100

    Publisher outline

--- project-ROS2-pkg/src/motor_ctrl_publisher.cpp
+++ project-ROS2-pkg/src/motor_ctrl_publisher.cpp
@@ -4,7 +4,7 @@
 #include <string>
 
 #include <rclcpp/rclcpp.hpp>
-#include "dynamixel_sdk_custom_interfaces/msg/set_position.hpp"
+#include <msg/set_position.hpp>
 
 using namespace std::chrono_literals;
 
@@ -15,15 +15,14 @@ class MotorPublisher : public rclcpp::Node
         {
             publisher_ = this->create_publisher<dynamixel_sdk_custom_interfaces::msg::SetPosition>("motor_ctrl",10);
             timer_ = this->create_wall_timer(
-                500ms, std::bind(&MotorPublisher::timer_callback, this));
-            
+                500ms, std::bind(&MotorPublisher::timer_callback, this));            
         }
 
     private:
     void timer_callback()
     {
       //auto message = std_msgs::msg::String();
-      //message.data = "Hello, world! " + std::to_string(count_++);
+      //message.data = "Hello, world! " + std::to_string(count_++);gitclone-b $ROS_DISTRO-develhttps://github.com/ROBOTIS-GIT/DynamixelSDK.git
       //RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
       //publisher_->publish(message);
     }
